5b552caa499749c60647032d443cb9aa23478870
Anna Peloušková <ann.pelouskova@gmail.com>
2025-10-22 14:06:57 +0200

    Temporarily allow all CORS origins for debugging - remove CORS restrictions

--- backend/src/app.js
+++ backend/src/app.js
@@ -57,18 +57,9 @@ app.use(inputSanitization);
 // CORS configuration podle cursorrules
 const corsOptions = {
     origin: function (origin, callback) {
-        const allowedOrigins = process.env.ALLOWED_ORIGINS ? 
-            process.env.ALLOWED_ORIGINS.split(',') : 
-            ['http://localhost:3000', 'http://localhost', 'https://srv-captain--unrollit', 'https://unrollit.aici.cz'];
-        
-        // Allow requests with no origin (mobile apps, Postman, etc.)
-        if (!origin) return callback(null, true);
-        
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+        // Dočasně povolím všechny origins pro debug
+        console.log('CORS origin check:', origin);
+        callback(null, true);
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
